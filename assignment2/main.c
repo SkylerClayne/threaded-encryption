f24d3a5b3a5dea3cc906998848a2b012a6871cf7
skylerclayne <skylerclayne@gmail.com>
2015-04-06 11:08:32 -0400

    Initial Commit

---
+++ assignment2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  assignment2
+//
+//  Created by Skyler Layne on 2015-04-06.
+//  Copyright (c) 2015 Skyler Layne. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
